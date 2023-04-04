36ddffee9db772daeeda12082d6d559f9c64d0ed
tsauvage <ssauvage.timothee@gmail.com>
2023-04-04 13:09:19 +0200

    fin exo tab

--- exercices/function.js
+++ exercices/function.js
@@ -1,58 +1,54 @@
-const exo1 = (nbr1, nbr2) => {
-    try {
-        return parseInt(nbr1) + parseInt(nbr2);
-    } catch (e) {
-        return false
-    }
+const ex1 = (nbr1, nbr2) => {
+    return parseInt(nbr1) + parseInt(nbr2)
 }
 
-const exo2 = (tab) => Math.max(...tab);
+const exo2 = (arr) => Math.max(...arr)
 
 const exo3 = (str) => {
     ['a', 'e', 'i', 'o', 'u','A','E','I','O','U'].forEach(element => str = str.replaceAll(element, ''))
 }
 
-const exo4 = (tab) => tab.sort()
+const exo4 = (arr) => arr.sort()
 
 const exo5 = (nbr) => {
-    strChiffre = ["Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf"]
-    strNombre = ["Onze", "Douze", "Treize", "Quatorze", "Quinze", "Seize", "dix-sept", "dix-huit", "dix-neuf"]
-    strBase = ["Vingt", "Trente", "Quarante", "Cinquante", "Soixante", "Soixante-Dix", "Quatre-Vingt", "Quatre-Vingt-Dix"]
+    strDecimal = ["Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf"]
+    strDecimal2 = ["Onze", "Douze", "Treize", "Quatorze", "Quinze", "Seize", "dix-sept", "dix-huit", "dix-neuf"]
+    strDecimalBase = ["Vingt", "Trente", "Quarante", "Cinquante", "Soixante", "Soixante-Dix", "Quatre-Vingt", "Quatre-Vingt-Dix"]
 
-    dizaine = parseInt(nbr / 10)
+    tensDecimal = parseInt(nbr / 10)
 
     if (dizaine > 0) {
-        base = nbr - dizaine * 10
+        base = nbr - tensDecimal * 10
         
-        if (dizaine === 1) {
-            return strNombre[base - 1]
+        if (tensDecimal === 1) {
+            return strDecimal2[base - 1]
         } else {
             if (dizaine !== 7 && dizaine !== 9) {
-                return strBase[dizaine - 2] + '-' + strChiffre[base-1]
+                return strDecimalBase[tensDecimal - 2] + '-' + strDecimal[base-1]
 
             } else {
-                return strBase[dizaine - 2] + '-' + strChiffre[base-1]
+                return strDecimalBase[tensDecimal - 2] + '-' + strDecimal[base-1]
             }
         }
     } else {
-       return strChiffre[nbr - 1]
+       return strDecimal[nbr - 1]
     }
 
 
 }
 
-const exo6 = (tab, key) => {
+const exo6 = (arr, key) => {
     let response = []
 
-    tab.forEach(element => {
+    arr.forEach(element => {
         response.push(element[key])    
     });
 
     return response
 }
 
-const exo7 = (tabNbr) => {
-    return tabNbr.sort((a,b) => {return b - a})
+const exo7 = (arrNbr) => {
+    return arrNbr.sort((a,b) => {return b.toLowerCase() - a.toLowerCase()})
 }
 
 const exo8 = (str) => {
@@ -64,15 +60,15 @@ const exo8 = (str) => {
 const exo9 = (str) => {
     let strLower = str.toLowerCase()
 
-    return strLower.match(new RegExp("[aeiouAEIOU]", 'g')).length
+    return strLower.match(new RegExp("[aeiou]", 'g')).length
 }
 
 const exo10 = (str) => {
-    let voy = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
     let response = ''
 
     for(let i = 0; i < str.length; i++) {
-        if (!voy.includes(str[i])) {
+        if (!vowel.includes(str[i])) {
             response += str[i].toUpperCase()
         } else {
             response += str[i]
@@ -82,13 +78,4 @@ const exo10 = (str) => {
     return response
 }
 
-let str = "zadsqioezjfe"
-let tabNbr = [1,2,4,32,3,4,5]
-let tabStr = ["aez",'salut', 'bonjour', 'tu es', "lol"]
-let tabTab = [
-    {"test" : "test", "test2": "test2"},
-    {"test" : "test", "test2": "test2"},
-    {"test" : "test", "test2": "test2"},
-    {"test" : "test", "test2": "test2"},
-]
 
--- exercices/tableaux.js
+++ exercices/tableaux.js
@@ -1,56 +1,95 @@
 //1
 
-// const arr1 = [1,2,3,4,5,6,7,8,9,10]
-// arr1.forEach(e => console.log(e))
+const arr1 = [1,2,3,4,5,6,7,8,9,10]
+arr1.forEach(e => console.log(e))
 
 
 //2
 
-// const semaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-// console.log(semaine[0])
+const week = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+console.log(week[0])
 
 
 //3
 
-// const arr3 = [2,4,6,8,10]
-// let count = 0
-// arr3.forEach(e => count += e)
-// console.log(count)
+const arr3 = [2,4,6,8,10]
+let count = 0
+arr3.forEach(e => count += e)
+console.log(count)
 
 
 //4
 
-// const fruits = ["Pêche", "Fraise", "Pomme", "Banane", "Kiwi"]
-// fruits.push("Orange")
+const fruits = ["Pêche", "Fraise", "Pomme", "Banane", "Kiwi"]
+fruits.push("Orange")
 
-// console.log(fruits)
+console.log(fruits)
 
 
 //5 
 
-// const note = [
-//     [2,4,12,17],
-//     [7,10,4],
-//     [10,10,10,10],
-//     [8,10,12],
-// ]
+const notes = [
+    [2,4,12,17],
+    [7,10,4],
+    [10,10,10,10],
+    [8,10,12],
+    [4,2,9,18,17,19]
+]
 
-// let temp = 0
+let temp = 0
 
-// note.forEach(eleve => {
-//     eleve.forEach(note => temp += note)
-//         console.log(temp / eleve.length)
-//         temp = 0
-// })
+notes.forEach(student => {
+    student.forEach(note => temp += note)
+    
+   console.log(temp / eleve.length)
+    temp = 0
+})
 
 //6 
 
-const oiseaux = ["Pigeon", "Moineau"]
+const birds = ["Pigeon", "Moineau"]
 
 target = "Pigeon"
 
-if (oiseaux.includes(target)) {
-    console.log("Inclu")
+if (birds.includes(target)) {
+    console.log(`${target} OK !`)
 } else {
-    console.log("Pas inclu")
+    console.log(`${target} NOT OK !`)
 }
+
+
+//7
+
+const countries = ["France", "Espagne", "Italie"]
+console.log(countries.map(country => country.toLowerCase()).sort())
+
+//8 
+
+const months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai"]
+console.log(months[2])
+
+//9
+const arr9 = []
+for (let i = 0; i < 10; i++) arr9.push(Math.floor(Math.random() * 100))
+
+console.log(Math.min(...arr8), Math.max(...arr8))
+
+//10
+const arr10 = [
+    "Je suis une phrase",
+    "Je suis une phrase un peu plus logue",
+    "Je suis une phr",
+    "Je suis unease",
+    "Je su phrase",
+    "Je suis eaezezaune phrase",
+]
+
+
+let strMaxLenght = ""
+arr9.forEach(str => {
+    if (str.length > strMaxLenght.length) {
+        strMaxLenght = str
+    }
+})
+
+console.log(strMaxLenght)
